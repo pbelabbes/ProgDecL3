c5488ec8d86930b525562b709e128b6cca671faf
Pierre Belabbes <pbelabbes@gmail.com>
2018-04-08 00:57:51 +0200

    add derivMult & derivSec

--- projet.pl
+++ projet.pl
@@ -1,16 +1,33 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   %%% debut de votre code ici %%%
 
+derivSec([X,X,X],[X,X]).
+derivSec([X,Y,X],[Y,X]).
 derivOne([X,Y,X,Y],[X,Y]).
 derivOne([X,X,X,X],[X,X]).
+
+%deriveSeconde
 derivOne([A,B,C,D],[X,Y]):-
+	{A = B-A},
+    {B = B-C},
+    {C = D-C},
+    derivSec([A,B,C],[Y,X]).
+             
+%derivermultiple
+derivOne([A,B,C,D],[X,Y]):-
+	{A = B/A},
+    {B = C/A},
+    {C = D/C},
+    derivMult([A,B,C],[V,W]),
+	    
+    .
 
 LogSuit([A,B,C,D,E],Y):-
     {A = B-A},
     {B = C-B},
     {C = D-C},
     {D = E-D },
-    dervOne([A,B,C,D],[X1,X2]),
+    derivOne([A,B,C,D],[X1,X2]),
     {Y = E + X2 }.
 
 
