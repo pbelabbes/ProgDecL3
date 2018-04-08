0a180d21060e97800efcc4d178e418d16d288936
Pierre Belabbes <pbelabbes@gmail.com>
2018-04-08 19:57:40 +0200

    add comments

--- projet.pl
+++ projet.pl
@@ -1,27 +1,48 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%	PROJET TEST D'INTELIGENCE %
+%	   By Pierre BELABBES     %
+%	   L3 MIAGE Grenoble      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%	        
 
+%SPEC
+%sub(A,B,X):true if A-B=X
 sub(A,B,X):-
     X is A - B.
 
+%SPEC
+%add(A,B,X):true if A+B=X
 add(A,B,X):-
     X is A + B.
 
+%SPEC
+%div(A;B,X):true if A/B=X
 div(A,B,X):-
     X is A / B.
 
+%SPEC
+%mult(A,B,X):true if A*B=X
 mult(A,B,X):-
     X is A * B.
 
+%SPEC
+%derivSec([A,B,C],X): true if A=B=C=X
 derivSec([X,X,X],X).
+
+%SPEC
+%derivSec([A,B,C],X): true if A=C and B=Y 
 derivSec([X,Y,X],Y).
 
-derivMult([X,X,X],X).
-derivMult([X,Y,X],Y).
-
-derivOne([X,Y,X,Y],X).
+%SPEC
+%derivOne([A,B,C,D],X): true if A=B=C=D=X
 derivOne([X,X,X,X],X).
 
+%%SPEC
+%derivOne([A,B,C,D],X): true if A=C=X and B=D
+derivOne([X,Y,X,Y],X).
 
-%deriveSeconde
+
+%SPEC
+%derivOne([A,B,C,D],X): true if X is the following element of the A,B,C,D suite thanks to an additionnal derivation
 derivOne([A,B,C,D],X):-
 	sub(B,A,E),
     sub(C,B,F),
@@ -29,17 +50,22 @@ derivOne([A,B,C,D],X):-
     derivSec([E,F,G],Y),
 	add(Y,D,X).	
              
-%derivermultiple
+%SPEC
+%derivOne([A,B,C,D],X): true if X is the following element of the A,B,C,D suite thanks to a product derivation
 derivOne([A,B,C,D],X):-
 	div(B,A,E),
     div(C,B,F),
     div(D,C,G),
-    derivMult([E,F,G],Y),
+    derivSec([E,F,G],Y),
     mult(D,Y,X).
 
+%SPEC
+%logSuit(L,Y): true if Y is the following element of the L logical consequence  
 logSuit([_|R],Y):-
     logSuit(R,Y).
 
+%SPEC
+%logSuit([A,B,C,D,E],Y): true if Y is the following element of the A,B,C,D,E logical consequence  
 logSuit([A,B,C,D,E],Y):-
     sub(B,A,F),
     sub(C,B,G),
