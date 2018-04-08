b8572aa68780160a34c7683c6db3cc4b86a1dae5
Pierre Belabbes <pbelabbes@gmail.com>
2018-04-08 18:25:57 +0200

    derivOne avec S1 ok

--- projet.pl
+++ projet.pl
@@ -1,17 +1,17 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
   %%% debut de votre code ici %%%
 
-derivSec([X,X,X],[X,X]).
-derivSec([X,Y,X],[Y,X]).
-derivOne([X,Y,X,Y],[X,Y]).
-derivOne([X,X,X,X],[X,X]).
+derivSec([X,X,X],X).
+derivSec([X,Y,X],Y).
+derivOne([X,Y,X,Y],X).
+derivOne([X,X,X,X],X).
 
 %deriveSeconde
-derivOne([A,B,C,D],[X,Y]):-
-	{A = B-A},
-    {B = B-C},
-    {C = D-C},
-    derivSec([A,B,C],[Y,X]).
+derivOne([A,B,C,D],X):-
+	A is B - A,
+    B is C - B,
+    C is D - C,
+    derivSec([A,B,C],X).
              
 %derivermultiple
 %derivOne([A,B,C,D],[X,Y]):-
@@ -20,18 +20,20 @@ derivOne([A,B,C,D],[X,Y]):-
  %   {C = D/C},
   %  derivMult([A,B,C],[V,W]).
 
+sub(A,B,X):-
+    X is A - B.
+
 logSuit([A,B,C,D,E],Y):-
-    {A = B-A},
-    {B = C-B},
-    {C = D-C},
-    {D = E-D },
-    derivOne([A,B,C,D],[_|X2]),
-    {Y = E + X2 }.
+    sub(B,A,F),
+    sub(C,B,G),
+    sub(D,C,H),
+    sub(E,D,I),
+    derivOne([F,G,H,I],X),
+   Y is E + X.
 
 
   %%% fin de votre code  ici  %%%
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 main :- % exemple de programme principal
   writeln('Donner un terme, un prédicat ? (avec un point à la fin de la ligne) '),
